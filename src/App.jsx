905067e5d04c7da2bca4b0e203c340ae4f60bbc2
Thanh Sang22 <thanhsang78202@gmail.com>
2024-04-01 19:50:27 +0700

    CRM-42 init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=" font-bold underline pt-3 text-3xl decoration-red-600">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
